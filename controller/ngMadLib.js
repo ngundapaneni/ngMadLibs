0d4522648c772f8c5a3a049ff9cde15b4b133783
Natasha <ngundapaneni@gmail.com>
2015-05-17 16:06:48 -0500

    adding a controller

--- controller/ngMadLib.js
+++ controller/ngMadLib.js
@@ -1,3 +1,13 @@
 angular.module('myApp', [])
-.controller("ngMadLibController", ['$scope'], function($scope) {
+.controller('ngMadLibController', function($scope) {
+	$scope.gender = 'Male';
+	$scope.name='Name'; 
+	$scope.jobTitle='Job Title'; 
+	$scope.tediousTask='Tedious Task'; 
+	$scope.dirtyTask='Dirty Task'; 
+	$scope.celebrity='Celebrity'; 
+	$scope.uselessSkill='Useless Skill'; 
+	$scope.adjective='Adjective'; 
+	$scope.obnoxiuousCelebrity='Obnoxiuous Celebrity'; 
+	$scope.hugeNumber='Huge Number';
 });
